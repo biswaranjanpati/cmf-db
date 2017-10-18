3e19f91a1719819c3fd80c8a805c87baaffb147d
biswaranjanpati <biswaranjanpati6804@gmail.com>
2017-10-18 14:39:22 +0530

    modified
    Multiple Entries added

--- main.js
+++ main.js
@@ -3,30 +3,41 @@ var db = firebase.firestore();
 
 db.collection("activities").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-        console.log(doc.id, " => ", doc.data());
+        //console.log(doc.id, " => ", doc.data());
+		console.log(doc.data().title);
+		abc();
     });
 });
 
+ 
 
 
 //Defining the DOM elements
-var addInput = document.querySelector("#addInput");
 var submitButton = document.querySelector("#submitButton");
 var listActivities = document.querySelector("#listActivities");
 
 //Attaching a listener function
-addInput.addEventListener('keyup', handleSubmit);
+//addInput.addEventListener('keyup', handleSubmit);
 submitButton.addEventListener('click', handleSubmit);
 
 //Setup Firestore Ref
 const activityRef = firebase.firestore().collection("activities");
+
+
 
 //Listener Function
 function handleSubmit(e) {
   if (e.keyCode !== 13 && e.type != "click") {
     return;
   }
-  const activity = addInput.value;
+  
+  //Reading Array when submitting
+  var values = $("input[name='report']")
+              .map(function(){return $(this).val();}).get();
+  var addInput = values;
+  
+	
+  const activity = addInput;
   submitButton.innerHTML = "Creating...";
   if (activity === "") {
     return;
@@ -36,12 +47,14 @@ function handleSubmit(e) {
     //checked: false,
     createdAt: (new Date()).getTime()
   }).then(function(docRef) {
-    addInput.value = "";
+    addInput = "";
     submitButton.innerHTML = "Create";
   }).catch(function(error) {
     console.log(error);
   })
 };
+
+
 
 // Listener for rendering todos
 activityRef.orderBy("createdAt", 'desc').onSnapshot(function(docSnapShot) {
@@ -52,7 +65,7 @@ activityRef.orderBy("createdAt", 'desc').onSnapshot(function(docSnapShot) {
 	  
 	  var date = activity.createdAt;
 	  var date = new Date(date);
-	  var date = new Date().getTime();
+	  var date = date.getDate() + "/" +  (date.getMonth() + 1) + "/" + date.getFullYear();
 
 	  
 	  var dateBlock = document.createElement("p");
@@ -68,13 +81,43 @@ activityRef.orderBy("createdAt", 'desc').onSnapshot(function(docSnapShot) {
     checkBox.checked = activity.checked;
     checkBox.addEventListener('change', handleCheckToggle);
     */
-    var titleBlock = document.createElement("p");
-    titleBlock.innerHTML = activity.title;
-    
+	  
+	  
+	  
+    var titleBlock = document.createElement("ul");
+    //titleBlock.innerHTML = activity.title;
+	//var list = document.getElementById("titleBlock");
+	  
+	  
+	  
+	  //console.log(activity);
+	  
+	  //console.log(activity.title);
+	  
+	  
+	 var abc = function (){
+		 var listEl = document.getElementById('listActivities');
 
+		var spellingList = activity.title;
+
+		var populateList = function (arr) {
+			var str = '';
+			for (var i = 0; i < arr.length; i++) {
+				str += '<li>' + arr[i] + '</li>';
+			}
+			return str;
+		}
+
+		listEl.innerHTML = populateList(spellingList);
+
+	 }
+	    
+	  
+	  
+	  
     var wrapper= document.createElement('li');
    // wrapper.appendChild(checkBox);
-    wrapper.appendChild(titleBlock);
+      //wrapper.appendChild(titleBlock);
 	  wrapper.appendChild(dateBlock);
 
     listActivities.appendChild(wrapper);
